ff91fcf78d882827d2d12dfa627c93dffe47dc42
shuo <shuo@shuodeMBP.wv.cc.cmu.edu>
2015-02-11 21:30:22 -0500

    first version

---
+++ SRC/get_predecessors.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "def.h"
+
+int get_predecessors(char *key, int k, char *result[]) {
+    key = NULL;
+    k = 0;
+    result = NULL;
+    printf("IMPLEMENT  ME\n");
+
+    return 0;
+}
---
+++ SRC/get_successors.c
@@ -0,0 +1,11 @@
+#include "def.h"
+
+
+int get_successors(char *key, int k, char *result[]) {
+    key = NULL;
+    k = 0;
+    result = NULL;
+    printf("IMPLEMENT  ME\n");
+
+    return 0;
+}
